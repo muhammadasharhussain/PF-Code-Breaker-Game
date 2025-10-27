f1fab772807515fb2c3236f99a63dc6842a8eb7f
Muhammad Ashar <145932531+muhammadasharhussain@users.noreply.github.com>
2025-10-27 20:12:38 +0500

    Add files via upload
    Incomplete Draft of 3 difficulty levels, Easy Medium & Hard.
    Correct logic, Next task is to combine the difficulty levels into a single code using switch case statement

---
+++ EasyDraft.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main(){
+    int i,j,k=0,z=0,attempt,score=115,level;
+    int randomNum[3]={0,6,8};
+    int num[3];
+ 
+  for(attempt=1;attempt<=5;attempt++){
+        printf("\nAttempt %d: " ,attempt);
+
+        scanf("%d%d%d",&num[0],&num[1],&num[2]);
+        z=0;
+            for(i=0;i<3;i++){
+                    if(num[i]==randomNum[i]){
+                        printf( " ✅");
+                        z++;
+                        if(z==3)
+                            break;
+                        continue;
+                    }
+                    for(j=0;j<3;j++){
+                        if(num[i]==randomNum[j]){
+                            k=1;
+                            break;
+                        }
+                    }
+                    if(k!=0)
+                        printf(" 🧐 ");
+                    else
+                        printf(" ❌ ");
+                    k=0;
+            }
+            score-=15;
+            if(z==3){
+                printf("\nYou Won!");
+                break;
+            }
+    }if(z!=3){
+        printf("\nYou Lost!");
+        score=0;
+    }   
+        printf(" Your score is %d",score);
+return 0;
+
+}
